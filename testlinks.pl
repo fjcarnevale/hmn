835607f7bc75dc535d6cc8d442deaa17b5a43d71
Frank J Carnevale <fcarnevale@CCCWORK3.WPI.EDU>
2013-01-29 09:04:13 -0500

    Can iterate names of links

--- testlinks.pl
+++ testlinks.pl
@@ -1,15 +1,20 @@
+#!/usr/bin/perl
 use LWP::Simple;
-use JSON qw( decode_json );
+use FindBin qw($Bin);
+use lib "$Bin/JSON-2.53/lib/";
+use JSON;
 use Data::Dumper;
 use strict;
 use warnings;
 
-my $url = "./js/objects.json";
+my $url = "http://users.wpi.edu/~fcarnevale/hmn/js/objects.json";
 
 my $json = get( $url );
 die "Could not get \"$url\"\n" unless defined $json;
 
-my $decoded_json = decode_json( $json );
+my $hash_ref = decode_json($json);
+my %json_hash = %$hash_ref;
 
-print Dumper $decoded_json;
-
+foreach my $key(keys %json_hash){
+	print $key,"\n";
+}
